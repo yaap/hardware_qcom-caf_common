f4f64fbfd9465389336c0a048b52eee87747076e
chaptsand <chaptsand@gmail.com>
2024-08-04 11:41:03 +0800

    Add conditional RFS install_symlink targets support
    The mpss firmware symlink_target of Samsung Qualcomm based devices is
    slightly different.
    
    This change is pretty much generic and allows for conditional
    configuration of these symlink targets.
    
    Change-Id: Ifaebc5c4aa0724007c169ec048d2f53f219e00b3

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -83,6 +83,16 @@ SOONG_CONFIG_qtidisplay_smmu_proxy ?= false
 SOONG_CONFIG_qtidisplay_ubwcp_headers ?= false
 SOONG_CONFIG_qtidisplay_composer_version ?= v2
 
+# Add rfs to soong config namespaces
+SOONG_CONFIG_NAMESPACES += rfs
+
+# Add supported variables to rfs config
+SOONG_CONFIG_rfs += \
+    mpss_firmware_symlink_target
+
+# Set default values for rfs config
+SOONG_CONFIG_rfs_mpss_firmware_symlink_target ?= firmware_mnt
+
 # Add rmnetctl to soong config namespaces
 SOONG_CONFIG_NAMESPACES += rmnetctl
 
