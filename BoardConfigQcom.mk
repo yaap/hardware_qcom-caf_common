cdffd05faa6761bb90e065e99d73cca2e5d4a0b1
Michael Bestas <mkbestas@lineageos.org>
2025-03-26 20:06:57 +0200

    common: move QTI gralloc configs to select
    Change-Id: I7d9dcdb8b4f0dcb34d390c048fb6eda90d721ba5

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -314,12 +314,21 @@ endif
 ifneq ($(filter $(UM_5_10_FAMILY) $(UM_5_15_FAMILY) $(UM_6_1_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE ?= true
     TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE ?= true
+else
+    TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE ?= false
+    TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE ?= false
 endif
 
 # Use QTI gralloc UBWCP struct
 ifneq ($(filter $(UM_6_1_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT ?= true
+else
+    TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT ?= false
 endif
+
+$(call soong_config_set,qtidisplay,gralloc_handle_has_custom_content_md_reserved_size,$(TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE))
+$(call soong_config_set,qtidisplay,gralloc_handle_has_reserved_size,$(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE))
+$(call soong_config_set,qtidisplay,gralloc_handle_has_ubwcp_format,$(TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT))
 
 ifneq ($(filter $(UM_3_18_HAL_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     MSM_VIDC_TARGET_LIST := $(UM_3_18_HAL_FAMILY)
